0d0044a52a14fac6aff862124049b285ae431d50
Chandler Tayek <chandlertayek@gmail.com>
2016-10-11 23:39:03 -0700

    feat: The Matrix

--- entertainment_center.py
+++ entertainment_center.py
@@ -10,6 +10,11 @@ avatar = media.Movie("Avatar",
                      "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
                      
-
-print(avatar.storyline)
-avatar.show_trailer()
+theMatrix = media.Movie("The Matrix",
+                        "A programmer turn super human to uncover the secrets of the matrix.",
+                        "http://www.imdb.com/title/tt0133093/mediaviewer/rm2882537984",
+                        "https://www.youtube.com/watch?v=vKQi3bBA1y8")
+                        
+#print(avatar.storyline)
+#avatar.show_trailer()
+theMatrix.show_trailer()
