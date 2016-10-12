1814dc161d8392e49f8b64eb48b8248264469608
Chandler Tayek <chandlertayek@gmail.com>
2016-10-12 12:13:27 -0700

    feat: Add more movies and fresh_tomatoe html generator

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,20 +1,36 @@
+import fresh_tomatoes
 import media
 
 toy_story = media.Movie("Toy Story", 
                         "A story of a boy and his toys that come to life",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 #print(toy_story.storyline)
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
                      
-theMatrix = media.Movie("The Matrix",
+the_matrix = media.Movie("The Matrix",
                         "A programmer turn super human to uncover the secrets of the matrix.",
-                        "http://www.imdb.com/title/tt0133093/mediaviewer/rm2882537984",
+                        "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
                         "https://www.youtube.com/watch?v=vKQi3bBA1y8")
-                        
-#print(avatar.storyline)
-#avatar.show_trailer()
-theMatrix.show_trailer()
+
+school_of_rock = media.Movie("School of Rock", "Storyline",
+                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille", "Storyline",
+                          "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight in Paris", "Storyline",
+                                "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
+                                
+hunger_games = media.Movie("Hunger Games", "Storyline",
+                           "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                           "https://www.youtube.com/watch?v=PbA63a7H0bo")
+                           
+movies = [toy_story, avatar, school_of_rock, the_matrix, ratatouille, midnight_in_paris, hunger_games]                           
+fresh_tomatoes.open_movies_page(movies)
